40250ac02892f6fc10e31eaee965d2aeca284217
Camilo Gomez <jcgomezj@eafit.edu.co>
2026-02-05 14:16:35 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
-
-def home (request):
-    return render(request, 'home.html',{'name':'Camilo Gomez'})
+from .models import Movie
 
 
-def about (request):
-    return render(request, 'movie/about.html')
+def home(request):
+    search_term = request.GET.get('searchMovie')  # <- igual al name del input
+
+    if search_term:
+        movies = Movie.objects.filter(title__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
+
+    return render(request, 'home.html', {
+        'searchTerm': search_term,
+        'movies': movies
+    })
+
+
+def about(request):
+    return HttpResponse('<h1>Welcome to the About Page</h1>')
